bf199f30f424b2ea20223576250d737bc3b378e8
Coroliov Oleg <1880059+ruscon@users.noreply.github.com>
2019-09-29 19:57:58 +0300

    test(here): fix integration test

--- test/fixture/provider/here.fixture.ts
+++ test/fixture/provider/here.fixture.ts
@@ -17,11 +17,11 @@ const providerRawLocationResponse: Readonly<any> = Object.freeze({
                         Location: {
                             LocationId: 'NT_.4F.1wbQ1kZP.RMjIh-x1A_xETN4A',
                             LocationType: 'point',
-                            DisplayPosition: { Latitude: 41.7332379, Longitude: -87.5959685 },
-                            NavigationPosition: [{ Latitude: 41.7331031, Longitude: -87.5959614 }],
+                            DisplayPosition: { Latitude: 41.7332449, Longitude: -87.5960168 },
+                            NavigationPosition: [{ Latitude: 41.73311, Longitude: -87.5960168 }],
                             MapView: {
-                                TopLeft: { Latitude: 41.734362, Longitude: -87.5974749 },
-                                BottomRight: { Latitude: 41.7321137, Longitude: -87.5944621 },
+                                TopLeft: { Latitude: 41.7343691, Longitude: -87.5975232 },
+                                BottomRight: { Latitude: 41.7321207, Longitude: -87.5945104 },
                             },
                             Address: {
                                 Label: '1158 E 89th St, Chicago, IL 60619, United States',
@@ -51,8 +51,8 @@ const providerRawLocationResponse: Readonly<any> = Object.freeze({
 
 const providerParsedLocationResponse: ReadonlyArray<LocationInterface> = Object.freeze<LocationInterface>([
     {
-        latitude: 41.7332379,
-        longitude: -87.5959685,
+        latitude: 41.7332449,
+        longitude: -87.5960168,
         country: 'United States',
         countryCode: 'US',
         state: 'Illinois',
@@ -109,23 +109,23 @@ export const providerRawPlaceDetailsResponse: Readonly<any> = Object.freeze({
                             LocationId: 'NT_.4F.1wbQ1kZP.RMjIh-x1A_xETN4A',
                             LocationType: 'address',
                             DisplayPosition: {
-                                Latitude: 41.7332379,
-                                Longitude: -87.5959685,
+                                Latitude: 41.7332449,
+                                Longitude: -87.5960168,
                             },
                             NavigationPosition: [
                                 {
-                                    Latitude: 41.7331031,
-                                    Longitude: -87.5959614,
+                                    Latitude: 41.73311,
+                                    Longitude: -87.5960168,
                                 },
                             ],
                             MapView: {
                                 TopLeft: {
-                                    Latitude: 41.734362,
-                                    Longitude: -87.5974749,
+                                    Latitude: 41.7343691,
+                                    Longitude: -87.5975232,
                                 },
                                 BottomRight: {
-                                    Latitude: 41.7321137,
-                                    Longitude: -87.5944621,
+                                    Latitude: 41.7321207,
+                                    Longitude: -87.5945104,
                                 },
                             },
                             Address: {

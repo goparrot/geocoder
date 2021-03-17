1e9beab3768400f896240093ac4cf5f337260342
Coroliov Oleg <1880059+ruscon@users.noreply.github.com>
2021-03-18 01:46:47 +0200

    ci: test repository-dispatch3

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,4 @@ export type Type<T> = new (...args: any[]) => T;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type Constructor<T extends object = {}> = new (...args: any[]) => T;
+////
